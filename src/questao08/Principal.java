a363bec6ebf4b69eb03fcb7cbf0c4656cebb4c97
Asphyxia404404 <crashwarrior501@gmail.com>
2025-08-04 21:59:51 -0300

    resolução da questao 8.

---
+++ src/questao08/Principal.java
@@ -0,0 +1,17 @@
+package questao08;
+// 8. Escrever um programa para exibir os números de 1 até 50 na tela usando o **for**.
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+
+        for(int i=0; i <= 50; i++){
+
+            System.out.printf("%d\n", i);
+        }
+
+
+    }
+
+}
