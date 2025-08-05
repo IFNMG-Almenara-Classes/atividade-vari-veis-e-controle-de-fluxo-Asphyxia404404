e2bcaf645cae1a1e130437a1917a4804f711c76e
Asphyxia404404 <crashwarrior501@gmail.com>
2025-08-04 22:04:59 -0300

    resolução da questao 9.

---
+++ src/questao09/Principal.java
@@ -0,0 +1,18 @@
+package questao09;
+// 9. Escrever um programa para exibir os números de 1 até 50 na tela usando o **while**.
+
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        int i=1;
+        
+        while(i <= 50){
+
+            System.out.printf("%d\n", i);
+            i++;
+        }
+    }
+    
+}
